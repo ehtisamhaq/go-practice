17b7b929962fc6a56478be14fbc2bb2675e79dc5
Eh-haque <ehtisamul@gmail.com>
2025-11-03 12:19:08 +0600

    loops

---
+++ hello/loops.go
@@ -0,0 +1,63 @@
+package main
+
+// loops.go
+
+import "fmt"
+
+func main() {
+	// for loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println("For Loop Iteration:", i)
+	}
+
+	// while-like loop
+	j := 1
+	for j <= 5 {
+		fmt.Println("While-like Loop Iteration:", j)
+		j++
+	}
+
+	// infinite loop with break
+	k := 1
+	for {
+		if k > 5 {
+			break
+		}
+		fmt.Println("Infinite Loop Iteration:", k)
+		k++
+	}
+	
+}
+
+func sumOfFirstN(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum
+}
+
+func factorial(n int) int {
+	result := 1
+	for i := 2; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
+
+func nestedLoopsExample() {
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 2; j++ {
+			fmt.Printf("i: %d, j: %d\n", i, j)
+		}
+	}
+}
+
+func loopWithContinue() {
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue // Skip even numbers
+		}
+		fmt.Println("Odd Number:", i)
+	}
+}	
