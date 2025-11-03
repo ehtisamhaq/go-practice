0928eedc504d6d3237ca89781fe5c031b1d8b781
Eh-haque <ehtisamul@gmail.com>
2025-11-03 11:55:28 +0600

    data types

---
+++ hello/data-types.go
@@ -0,0 +1,33 @@
+package main
+
+// integer types
+var intVar int = 10
+var int8Var int8 = -128
+var int16Var int16 = 32767
+var int32Var int32 = 2147483647
+var int64Var int64 = 9223372036854775807
+
+// unsigned integer types
+var uintVar uint = 20
+var uint8Var uint8 = 255
+var uint16Var uint16 = 65535
+var uint32Var uint32 = 4294967295
+var uint64Var uint64 = 18446744073709551615
+
+// floating-point types
+var float32Var float32 = 3.14
+var float64Var float64 = 3.141592653589793
+
+// boolean type
+var boolVar bool = true
+
+// string type
+var stringVar string = "Hello, Go!"
+
+// complex types
+var complex64Var complex64 = complex(1, 2)
+var complex128Var complex128 = complex(3, 4)
+
+// rune and byte types
+var runeVar rune = 'G'
+var byteVar byte = 'A'
