f18c2b104e8d8ed4c558af7887c2e39d5af5113f
Eh-haque <ehtisamul@gmail.com>
2025-11-03 12:00:09 +0600

    constants

---
+++ hello/constants.go
@@ -0,0 +1,21 @@
+package main
+
+const (
+	PI       = 3.14
+	AppName  = "GoApp"
+	Version  = "1.0.0"
+	MaxUsers = 100
+)	
+
+
+
+const appName string= "Ehtisam"
+const age = 26
+
+
+// iota example
+const (
+	Red = iota
+	Green
+	Blue
+)
