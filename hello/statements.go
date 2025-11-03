7aef38f183aaa7a2a02efe607624d83b6b967411
Eh-haque <ehtisamul@gmail.com>
2025-11-03 12:14:05 +0600

    statements

--- hello/statements.go
+++ hello/statements.go
@@ -16,3 +16,25 @@ func main() {
 	fmt.Println(checkEvenOdd(10)) // Output: Even
 	fmt.Println(checkEvenOdd(7))  // Output: Odd
 }
+
+// switch statement
+func dayOfWeek(day int) string {
+	switch day {
+	case 1:
+		return "Monday"
+	case 2:
+		return "Tuesday"
+	case 3:
+		return "Wednesday"
+	case 4:
+		return "Thursday"
+	case 5:
+		return "Friday"
+	case 6:
+		return "Saturday"
+	case 7:
+		return "Sunday"
+	default:
+		return "Invalid day"
+	}
+}
